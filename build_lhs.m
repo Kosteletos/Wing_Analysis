b5b8273abd1310b4314877e95fc21c9144a1677e
Tom Kosteletos <tk520@cam.ac.uk>
2020-05-12 16:27:23 +0100

    Build_rhs vectorised and build_lhs partially vectorised

--- build_lhs.m
+++ build_lhs.m
@@ -4,33 +4,41 @@ function lhsmat = build_lhs(xs,ys)
 np = length(xs) - 1;
 psip = zeros(np,np+1); 
 
+
+
 % calculates intermediate psi matrix, the i indictes the row of the matrix
 % outside of the j loop j = np+1 and j = 1 cells are filled and initial j-1
 % value is calculated. The panels are then itterated over to fill their
 % corresponding cell
 
-for i= 1:np
-    [infa, infb] =  panelinf(xs(1),ys(1),xs(2),ys(2),xs(i),ys(i));
-    psip(i,1) = infa;
-    inf_b_dummy = infb;
+%%%NEW%%% But doing nothing at the moment
+xs_shift = circshift(xs,-1); % Shifted xs == x(i+1)
+ys_shift = circshift(ys,-1); % Shifted ys == y(i+1)
+%%%NEW%%%
+
+[infa, infb] =  panelinf(xs(1),ys(1),xs(2),ys(2),xs(1:np),ys(1:np));
+psip(:,1) = infa;
+inf_b_dummy = infb;
     
-    [infa, infb] =  panelinf(xs(np+1),ys(np+1),xs(2),ys(2),xs(i),ys(i));
-    psip(i,np+1) = infb;
-    
-    for j = 2:np
-        [infa, infb] =  panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xs(i),ys(i));
-        psip(i,j) = infa + inf_b_dummy;
-        inf_b_dummy=infb;
-    end
+[~, infb] =  panelinf(xs(np+1),ys(np+1),xs(2),ys(2),xs(1:np),ys(1:np));
+psip(:,np+1) = infb;
+
+
+for j = 2:np
+    [infa, infb] =  panelinf(xs(j),ys(j),xs(j+1),ys(j+1),xs(1:np),ys(1:np));
+    psip(:,j) = infa + inf_b_dummy;
+    inf_b_dummy=infb;
 end
 
+%%%NEW%%% but doing nothing at the moment
+[infa_test, infb_test] = panelinf(xs,ys,xs_shift,ys_shift,xs,ys);
+%%%%%%%%%
+
+% Build A (lhsmat) from psi_p adn a shifted Psi_p
 lhsmat = zeros(np+1,np+1);
-for i= 1:np-1
-    for j = 1:np+1
-    lhsmat(i,j) = psip(i+1,j) - psip(i,j);    
-    end    
-end
 
+psip_shift = circshift(psip,-1,1); % Shifted psip == psip(i+1,j)
+lhsmat(1:np-1,:) = psip_shift(1:np-1,:)-psip(1:np-1,:); %these dimensions to leave final rows zeros
 lhsmat(np,1) = 1;
 lhsmat(np+1,np+1) = 1;
 
--- panelinf.m
+++ panelinf.m
@@ -2,19 +2,25 @@ function [infa, infb] = panelinf(xa,ya,xb,yb,x,y)
 % calculates influence coefficients infa, infb at all x,y for panel
 % between (xa,ya) and (xb,yb)
 
-t = [xb-xa,yb-ya];
-t = t/norm(t);
+tx = xb-xa;
+ty = yb-ya;
+t_norm = (tx.^2 + ty.^2).^0.5;
+tx = tx./t_norm;
+ty = ty./t_norm;
 
-n = [ya-yb,xb-xa];
-n = n/norm(n);
+nx = ya-yb;
+ny = xb-xa;
+n_norm = (nx.^2 + ny.^2).^0.5;
+nx = nx./n_norm;
+ny = ny./n_norm;
 
 rx = x-xa;
 ry = y-ya;
 
-X = rx.*t(1) + ry.*t(2);
-Y = rx.*n(1) + ry.*n(2);
+X = rx.*tx + ry.*ty;
+Y = rx.*nx + ry.*ny;
 
-del = sqrt((xb-xa)^2+(yb-ya)^2);
+del = ((xb-xa).^2+(yb-ya).^2).^0.5;
 
 [infa, infb] = refpaninf(del,X,Y);
 end
