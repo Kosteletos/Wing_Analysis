8d61a3ecafeee75ce138de80cd476d4f180804de
PaulCumner <65023067+PaulCumner@users.noreply.github.com>
2020-05-26 22:25:03 +0100

    new runs

--- week 3,4/efficient_plotting.m
+++ week 3,4/efficient_plotting.m
@@ -4,14 +4,14 @@ clear all
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-data_list = {'low62','low61','low6'};    % what to plot
-angle_list = {'0','3'};                      % Angles to plot cp at
+data_list = {'l30702','l308','l307','l307f3','l30703','l30706','l30704'};    % what to plot
+angle_list = {'0','5'};                      % Angles to plot cp at
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 
 
-colour_array = {'b','g','r','c','m','k'};
+colour_array = {'b','r','g','k','m','c','k'};
 data_len = numel(data_list);
 angle_len = numel(angle_list);
 
@@ -20,7 +20,7 @@ figure('Name','L/D vs \alpha')
 hold on
 for i = 1:data_len
    data = load(strcat('Data/',data_list{i}));
-   plot(data.alpha,data.lovdswp,'-' ,'color',colour_array{mod(i,6)},'linewidth',1.5)
+   plot(data.alpha,data.lovdswp,'-' ,'color',colour_array{mod(i,8)},'linewidth',1.5)
 end
 legend(data_list,'location','southeast')
 hold off
@@ -30,7 +30,7 @@ figure('Name','C_D vs C_L ')
 hold on
 for i = 1:data_len
    data = load(strcat('Data/',data_list{i}));
-   plot(data.clswp,data.cdswp,'-' ,'color',colour_array{mod(i,6)},'linewidth',1.5)
+   plot(data.clswp,data.cdswp,'-' ,'color',colour_array{mod(i,8)},'linewidth',1.5)
 end
 legend(data_list,'location','northwest')
 hold off
@@ -40,7 +40,7 @@ figure('Name','Cl vs \alpha ')
 hold on
 for i = 1:data_len
    data = load(strcat('Data/',data_list{i}));
-   plot(data.alpha,data.clswp,'-' ,'color',colour_array{mod(i,6)},'linewidth',1.5)
+   plot(data.alpha,data.clswp,'-' ,'color',colour_array{mod(i,8)},'linewidth',1.5)
 end
 legend(data_list,'location','northwest')
 hold off
@@ -50,7 +50,7 @@ figure('Name','C_D vs alpha')
 hold on
 for i = 1:data_len
    data = load(strcat('Data/',data_list{i}));
-   plot(data.alpha,data.cdswp,'-' ,'color',colour_array{mod(i,6)},'linewidth',1.5)
+   plot(data.alpha,data.cdswp,'-' ,'color',colour_array{mod(i,8)},'linewidth',1.5)
 end
 legend(data_list,'location','northwest')
 hold off
@@ -62,7 +62,7 @@ for i = 1:angle_len
     hold on
     for j = 1:data_len
         data = load(strcat('Data/',data_list{j},'_',angle_list{i}));
-        plot(data.xs,data.cp,'-' ,'color',colour_array{mod(j,6)},'linewidth',1)
+        plot(data.xs,data.cp,'-' ,'color',colour_array{mod(j,8)},'linewidth',1.1)
     end
     legend(data_list,'location','northwest')
     hold off
