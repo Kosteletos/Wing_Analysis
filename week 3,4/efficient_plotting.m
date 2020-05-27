ad2da246aad43dbd5cbc96af86eeb48c0f743651
PaulCumner <65023067+PaulCumner@users.noreply.github.com>
2020-05-27 12:23:41 +0100

    new tests

--- week 3,4/efficient_plotting.m
+++ week 3,4/efficient_plotting.m
@@ -4,8 +4,8 @@ clear all
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-data_list = {'l30702','l308','l307','l307f3','l30703','l30706','l30704'};    % what to plot
-angle_list = {'0','5'};                      % Angles to plot cp at
+data_list = {'l30703c','l30703c-1','l30703c-2','l30703c-3','l30703'};    % what to plot
+angle_list = {'0','2','4'};                      % Angles to plot cp at
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
@@ -64,7 +64,7 @@ for i = 1:angle_len
         data = load(strcat('Data/',data_list{j},'_',angle_list{i}));
         plot(data.xs,data.cp,'-' ,'color',colour_array{mod(j,8)},'linewidth',1.1)
     end
-    legend(data_list,'location','northwest')
+    legend(data_list,'location','southeast')
     hold off
 end
 
