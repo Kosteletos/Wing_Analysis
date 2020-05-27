077a6cfd4b606b9f52bfa80a0765aed05a83bf84
PaulCumner <65023067+PaulCumner@users.noreply.github.com>
2020-05-27 16:36:28 +0100

    start on high

---
+++ week 3,4/geometry_plotting.m
@@ -0,0 +1,21 @@
+close all
+clear all
+
+data_list = {'h3','h4'};    % what to plot
+
+
+data_len = numel(data_list);
+figure('Name','Geometry')
+hold on
+for i = 1:data_len
+    y=load(strcat('Geometry/',data_list{i},'.surf'));
+    x=y(2:end-1,1);
+    y=y(2:end-1,2);
+
+    [xs ys] = splinefit([1;x;1],[0;y;0],0);
+    plot(xs,ys)
+end
+legend(data_list,'location','southeast')
+hold off
+axis equal
+axis([0 1 -0.2 0.2])
